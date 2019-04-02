2e7752fc168e12412434c97c096ed93be33500c7
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2019-04-02 12:01:10 +0700

    adding progress bar

--- app/src/main/java/com/example/ridwanzal/justjava/MainActivity.java
+++ app/src/main/java/com/example/ridwanzal/justjava/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.ridwanzal.justjava;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar webProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_web);
         WebView webView = (WebView) findViewById(R.id.webview);
+        webProgress = findViewById(R.id.progressBar);
+        webProgress.setMax(100);
+        webProgress.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#ffda09")));
+
         webView.getSettings().setLoadsImagesAutomatically(true);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
@@ -24,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setAppCacheEnabled(true);
         webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
         webView.getSettings().setEnableSmoothTransition(true);
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                webProgress.setProgress(newProgress);
+            }
+        });
 
         // Tiga baris di bawah ini agar laman yang dimuat dapat
         // melakukan zoom.
